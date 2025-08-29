b57f7bc6e6f86d9f0c436111be078ec84bdf04b3
Abhiram <abhiramvsa7@gmail.com>
2025-08-30 00:48:23 +0530

    feat: Implement user controller and routes for fetching users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import { getUsers } from "../controllers/user.controller.js";
+import { getUser } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get('/users', (req, res) => res.send({title: 'GET all users'}));
-userRouter.get('id', (req, res) => res.send({title: 'GET user details'}));
+userRouter.get('/users', getUsers);
+userRouter.get('id', getUser);
 userRouter.post('/', (req, res) => res.send({title: 'CREATE new user'}));
 userRouter.put('/', (req, res) => res.send({title: 'UPDATE user'}));
 userRouter.delete('/', (req, res) => res.send({title: 'DELETE user'}));
